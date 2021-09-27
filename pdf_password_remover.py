5926b2021533dda0934d693d23d99cb1cdf59281
thomasdelorge <90192744+thomasdelorge@users.noreply.github.com>
2021-09-27 22:29:24 +0200

    Add comments

--- pdf_password_remover.py
+++ pdf_password_remover.py
@@ -2,12 +2,13 @@ import pikepdf
 import fnmatch
 import os
 
+#Edit "0" with you own password, must be the same for all PDF files
 pdf_password = "0"
 
-#filter only .pdf files
+#Filter PDF files only
 files_in_path = fnmatch.filter(os.listdir("."), "*.pdf")
 
-#for each .pdf files, open then save it without password
+#For each .pdf files, open then save it without password
 for files in files_in_path:
     pdf = pikepdf.open(".\\" + files, password=pdf_password, allow_overwriting_input=True)
     pdf.save(".\\" + files)
