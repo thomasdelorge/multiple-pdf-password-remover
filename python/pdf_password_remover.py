a1da55e1a2383cf8f44e7edbc8c688d65a8e7b96
overnite22 <80704084+overnite22@users.noreply.github.com>
2023-05-24 03:29:00 +0200

    working on python 3.8.5

--- python/pdf_password_remover.py
+++ python/pdf_password_remover.py
@@ -3,22 +3,21 @@ import pikepdf
 import logging
 import coloredlogs
 
-#CHANGE PASSWORD HERE
-pdf_password = "0"
+# CHANGE PASSWORD HERE
+pdf_password = "changeme"
 
-#Logs to scriptName.log
+# Logs to scriptName.log
 scriptName = os.path.basename(__file__)
-logging.basicConfig(level = logging.DEBUG,
-                    filename = scriptName + ".log",
-                    filemode = "a",
-                    encoding = 'utf-8',
-                    format = '[%(asctime)s] [%(levelname)s] %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    filename=scriptName + ".log",
+                    filemode="a",
+                    format='[%(asctime)s] [%(levelname)s] %(message)s')
 
-#Logs into console
+# Logs into console
 mylogs = logging.getLogger(__name__)
 stream = logging.StreamHandler()
 mylogs.addHandler(stream)
-coloredlogs.install(level=logging.DEBUG, 
+coloredlogs.install(level=logging.DEBUG,
                     logger=mylogs,
                     fmt='[%(asctime)s] [%(levelname)s] %(message)s')
 
@@ -29,19 +28,20 @@ for subdir, dirs, files in os.walk(rootdir):
         filepath = subdir + os.sep + file
         if filepath.endswith(".pdf"):
             nb += 1
-            mylogs.info(str(nb) + ") File processing : " + file + " (" + filepath +")")
-            #Check if th PDF is password locked
-            try: 
+            mylogs.info(str(nb) + ") File processing : " + file + " (" + filepath + ")")
+            # Check if the PDF is password locked
+            try:
                 pdf = pikepdf.open(filepath)
-                mylogs.info(file + " isn't lock with a password")
-            except pikepdf._qpdf.PasswordError:
-                #If locked, try to unlock with password line 7
+                mylogs.info(file + " isn't locked with a password")
+            except pikepdf.PasswordError:
+                # If locked, try to unlock with password line 7
                 try:
                     pdf = pikepdf.open(filepath, password=pdf_password, allow_overwriting_input=True)
                     pdf.save(filepath)
-                    mylogs.info ("Successfully remove password on " + file)
-                except pikepdf._qpdf.PasswordError:
-                    mylogs.error ("Bad password for " + file)
-                except: #default
-                    mylogs.error ("Failed to remove password on " + file)
-os.system("pause")
+                    mylogs.info("Successfully remove password on " + file)
+                except pikepdf.PasswordError:
+                    mylogs.error("Bad password for " + file)
+                except:  # default
+                    mylogs.error("Failed to remove password on " + file)
+
+input("Press Enter to continue...")
